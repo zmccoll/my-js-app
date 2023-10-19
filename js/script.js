d0044e90695bc22526ff25d6904ea0b04137131c
zmccoll <zachmccoll@gmail.com>
2023-10-18 23:29:40 -0700

    update script.js
    added an array of objects with pokemon data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert('hello world');
-
-let favFood = 'Pizza';
-document.write(favFood);
+let pokemonList = [
+    {name: 'Pikachu', height: 1, types: 'electric'},
+    {name: 'Oddish', height: 0.5, types: ['grass', 'poison']},
+    {name: 'Koffing', height: 0.6, types: 'poison'},
+];
