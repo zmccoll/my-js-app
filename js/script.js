86c777f00037736dcc49fa7869dd041a2944c290
zmccoll <zachmccoll@gmail.com>
2023-10-21 23:05:58 -0700

    update script.js
    changed some info about pikachu and added a for loop to display all of the pokemon info on the page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
+//Pokemon List
 let pokemonList = [
-    {name: 'Pikachu', height: 1, types: 'electric'},
+    {name: 'Pikachu', height: 0.5, types: 'electric'},
     {name: 'Oddish', height: 0.5, types: ['grass', 'poison']},
     {name: 'Koffing', height: 0.6, types: 'poison'},
 ];
+//loop to display all of the pokemon information
+for (i = 0; i < pokemonList.length; i++) {
+    document.write('<p>', pokemonList[i].name + "(height: " + pokemonList[i].height + ")", '</p>');
+}
