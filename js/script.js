13a2702d080b1b1ff3cf10609613655b0672186d
zmccoll <zachmccoll@gmail.com>
2023-11-03 17:17:56 -0700

    update script.js
    changed for each loop and established it as a function in my IIFE

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
 let pokemonRepository = (function(){
     //Pokemon List
-    let pokemonList = [
+    let pokedex = [
     {name: 'Pikachu', height: 0.5, types: 'electric'},
     {name: 'Oddish', height: 0.5, types: ['grass', 'poison']},
     {name: 'Koffing', height: 0.6, types: 'poison'},
     ];
     
     function getAll () {
-       return pokemonList;
+       return pokedex;
     };
 
-    function add () {
-        pokemonList.push(Pokemon);
+    function add (pokemon) {
+        pokedex.push(pokemon);
+    };
+
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listItem= document.createElement('li');
+        let button = document.createElement('butotn');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem);
     }
+
     return {
         getAll : getAll,
-        add : add
-}
+        add : add,
+        addListItem: addListItem
+};
 })();
- 
+console.log(pokemonRepository.getAll());
+    
+
 
 /*
 //loop to display all of the pokemon information
@@ -31,15 +45,22 @@ for (i = 0; i < pokemonList.length; i++) {
 }*/
 
 //forEach()
-pokemonRepository.getAll().forEach(function(pokemonList) {
-    document.write("<p>", pokemonList.name + "(height: " + pokemonList.height + ")", "</p>");
-    //condition commenting whether pokemon is big or not
-    if (pokemonList.height >= 0.6) {
-        document.write(" - That's a decent sized Pokemon!");
-    }
+pokemonRepository.getAll().forEach(function(pokedex) {
+    pokemonRepository.addListItem(pokedex);
 }); 
 
-//test function
+/*pokemonRepository.getAll().forEach(function(pokedex) {
+    //info display
+    document.write("<p>", pokedex.name + " (height: " + pokedex.height + ")", "</p>");
+    //condition commenting whether pokemon is big or not
+    if (pokedex.height >= 0.6) {
+        document.write(" - That's a decent sized Pokemon!");
+    let pokemonList = document.querySelector('pokemon-list')
+    }
+}); 
+*/
+
+/*test function
 function divide(dividend, divisor) {
     if(divisor === 0) {
         return "you're trying to divide by 0";
@@ -53,3 +74,5 @@ console.log(divide(4,2));
 console.log(divide(7,0));
 console.log(divide(1,4));
 console.log(divide(12,-3));
+
+*/
